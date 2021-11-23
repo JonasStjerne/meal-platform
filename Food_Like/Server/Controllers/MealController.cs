e51b586e2904f9cf418254143189f3ca42185a96
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2021-11-23 12:07:40 +0100

    Create user

--- Food_Like/Server/Controllers/MealController.cs
+++ Food_Like/Server/Controllers/MealController.cs
@@ -16,23 +16,23 @@ namespace Food_Like.Server.Controllers
     {
         private static readonly HttpClient client = new HttpClient();
 
-        [HttpGet("search")]
-        public List<Meal> Search(string location)
+        [HttpGet("search{location}")]
+        public dynamic Search(string location)
         {
             using (var context = new foodlikeContext())
             {
 
-                //List<Meal> meals = context.Meal.ToList();
-                //Dictionary<Meal, int> mealDistance = new Dictionary<Meal, int>();
-                //foreach (var meal in meals)
-                //{
-                //    Seller seller = context.Seller.Where(e => e.SellerId == meal.SellerId).First();
-                //    Address sellerAddress = context.Address.Where(e => e.AddressId == seller.AddressId).First();
-                //    dynamic distanceToMeal = client.GetAsync(String.Format("https://maps.googleapis.com/maps/api/distancematrix/json?destinations={0}&origins={1}&key=AIzaSyBlObeK6EyBueLnbUSnlf1QO45xaO0wxro", sellerAddress, location));
-                //    mealDistance.Add(meal, distanceToMeal.elements[0].distance.value);
-                //}
-                //return mealDistance;
-                return context.Meal.ToList();
+                List<Meal> meals = context.Meal.ToList();
+                Dictionary<Meal, int> mealdistance = new Dictionary<Meal, int>();
+                foreach (var meal in meals)
+                {
+                    Seller seller = context.Seller.Where(e => e.SellerId == meal.SellerId).First();
+                    Address selleraddress = context.Address.Where(e => e.AddressId == seller.AddressId).First();
+                    dynamic distancetomeal = client.GetAsync(string.Format("https://maps.googleapis.com/maps/api/distancematrix/json?destinations={0}&origins={1}&key=aizasyccdwwvhnejhbnscxgv1ik3boqdwtg0bt0", selleraddress, location));
+                    mealdistance.Add(meal, distancetomeal.elements[0].distance.value);
+                }
+                return mealdistance;
+                //return context.Meal.ToList();
             }
         }
 
---
+++ Food_Like/Shared/auth/CreateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Food_Like.Shared
+{
+    public class CreateRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Mobile { get; set; }
+        public string ProfilePicture { get; set; }
+
+    }
+}
