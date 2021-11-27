d7609bb8a18ffbe7bcc529e10cba109f0295e475
Frederikke Filtenborg Michaelsen <frederikke.fm@icloud.com>
2021-11-27 15:27:14 +0100

    Efter merge

--- Food_Like/Server/db/testdata.sql
+++ Food_Like/Server/db/testdata.sql
@@ -1,1 +1,133 @@
-﻿
+﻿INSERT INTO Buyer 
+VALUES (
+"Hanne",
+"Jensen",
+"Hanne.Jensen@gmail.com",
+"12345678",
+LOAD_FILE('/Users/Frederikke/Projects/P3/Food_Like/Client/wwwroot/assets/Hanne.png'),
+"abc123",
+);
+
+INSERT INTO Buyer 
+VALUES (
+"Lars",
+"Hansen",
+"Lars.Hansen@gmail.com",
+"98765432",
+...png,
+"mad2021",
+);
+
+INSERT INTO Address 
+VALUES (
+"Boulevarden 14",
+"Aalborg",
+);
+
+INSERT INTO Address 
+VALUES (
+"Strandvejen 16",
+"Aalborg",
+);
+
+INSERT INTO Seller 
+VALUES (
+png,
+);
+
+INSERT INTO Seller 
+VALUES (
+png,
+);
+
+
+INSERT INTO Meal 
+VALUES (
+"Spagetti w/chicken",
+5,
+20,
+"Spagetti with chicken and thomato",
+"Spaghetti,Tomato, Mozzarella, Chicken Breast,Balsamic,Garlic,Rosemary,Pesto",
+2021, 1, 20, 19, 0, 0,
+2021, 1, 20, 19, 20, 0,
+png,
+);
+
+INSERT INTO Meal 
+VALUES (
+"Spagetti w/Beef",
+1,
+20,
+"Spagetti with beef and thomato",
+"Spaghetti,Tomato, Mozzarella, beef,Balsamic,Garlic,Rosemary,Pesto",
+2021, 1, 20, 19, 0, 0,
+2021, 1, 20, 19, 20, 0,
+png,
+);
+
+INSERT INTO Meal 
+VALUES (
+"Spagetti w/Meatball",
+2,
+20,
+"Spagetti with meatball and thomato",
+"Spaghetti,Tomato, Mozzarella, meatball,Balsamic,Garlic,Rosemary,Pesto",
+2021, 1, 20, 19, 0, 0,
+2021, 1, 20, 19, 20, 0,
+png,
+);
+
+
+
+
+INSERT INTO Review 
+VALUES (
+5
+"Fantastic"
+);
+
+INSERT INTO Review 
+VALUES (
+1
+"Disappointing"
+);
+
+INSERT INTO Review 
+VALUES (
+3
+"Okay"
+);
+
+INSERT INTO Review 
+VALUES (
+2
+"Not the best"
+);
+
+INSERT INTO Review 
+VALUES (
+4
+"Super"
+);
+
+INSERT INTO Category 
+VALUES (
+"Danish"
+);
+
+INSERT INTO Category 
+VALUES (
+"American"
+);
+INSERT INTO Category 
+VALUES (
+"Italien"
+);
+INSERT INTO Category 
+VALUES (
+"Asian"
+);
+INSERT INTO Category 
+VALUES (
+"Spanish"
+);
