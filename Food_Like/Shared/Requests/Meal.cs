6765cb8f99a70ba7c9eae6465370575f9aae9b6a
Frederikke Filtenborg Michaelsen <frederikke.fm@icloud.com>
2021-12-02 20:00:26 +0100

    final styling og funktionalitet

---
+++ Food_Like/Shared/Requests/Meal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Food_Like.Shared
+{
+    public partial class Meal
+    {
+        [NotMapped]
+        public int Reserved { get; set; }
+    }
+}
---
+++ Food_Like/Shared/Requests/Seller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Food_Like.Shared
+{
+    public partial class Seller
+    {
+        [NotMapped]
+        public decimal Rating { get; set; }
+    }
+}
